5a757ae09390b74a7bdca351ef8a138f4a2f474b
Tom Haskins-Vaughan <tomhv@users.noreply.github.com>
2019-01-14 21:18:48 -0500

    [api#4] Exposing basic Recipe

--- src/Entity/Recipe.php
+++ src/Entity/Recipe.php
@@ -2,14 +2,32 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Validator\Constraints as Assert;
+
 /**
- * @author @CountDucku
+ * @author CountDucku
  * @since  1.0.0
+ *
+ * @ApiResource
+ * @ORM\Entity
  */
 class Recipe
 {
     /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    public $id;
+
+    /**
      * @var string
+     *
+     * @ORM\Column
+     * @Assert\NotBlank
      */
     private $name;
 
@@ -21,5 +39,10 @@ class Recipe
     public function getName(): string
     {
         return $this->name;
+    }
+
+    public function getId()
+    {
+        return $this->id;
     }
 }
